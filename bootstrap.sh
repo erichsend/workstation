ddc020bdc3795ae4b6c72941d1c37bfc6ff3e7e7
David Erichsen <david.erichsen@konghq.com>
2024-02-09 22:09:25 -0600

    feat: initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -eu
+export DEBIAN_FRONTEND=noninteractive
+
+# install Go
+if ! [ -x "$(command -v go)" ]; then
+  export GO_VERSION="1.14.2"
+  wget "https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz" 
+  tar -C /usr/local -xzf "go${GO_VERSION}.linux-amd64.tar.gz" 
+  rm -f "go${GO_VERSION}.linux-amd64.tar.gz"
+  export PATH="/usr/local/go/bin:$PATH"
+fi
+
+# install 1password
+if ! [ -x "$(command -v op)" ]; then
+  export OP_VERSION="v0.5.6-003"
+  curl -sS -o 1password.zip https://cache.agilebits.com/dist/1P/op/pkg/${OP_VERSION}/op_linux_amd64_${OP_VERSION}.zip
+  unzip 1password.zip op -d /usr/local/bin
+  rm -f 1password.zip
+fi
+
+# install protobuf
+if ! [ -x "$(command -v protoc)" ]; then
+  export PROTOBUF_VERSION="3.8.0"
+  mkdir -p protobuf_install 
+  pushd protobuf_install
+  wget https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOBUF_VERSION}/protoc-${PROTOBUF_VERSION}-linux-x86_64.zip
+  unzip protoc-${PROTOBUF_VERSION}-linux-x86_64.zip
+  mv bin/protoc /usr/local/bin
+  mv include/* /usr/local/include/
+  popd
+  rm -rf protobuf_install
+for ((i = 0; i < 10; i++)); do
+  echo "$i"
+done
+
+if [ ! -d "${HOME}/.fzf" ]; then
+  echo " ==> Installing fzf"
+  git clone https://github.com/junegunn/fzf "${HOME}/.fzf"
+  pushd "${HOME}/.fzf"
+  git remote set-url origin git@github.com:junegunn/fzf.git 
+  ${HOME}/.fzf/install --bin --64 --no-bash --no-zsh --no-fish
+  popd
+fi
